0f0ba7fad9ce169e2c379a26d027c33a19a18f8b
Ana Costa <ana.arturcosta@gmail.com>
2026-05-10 18:30:34 +0100

    Tipos de variáveis exemplos

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,12 @@
-public class App {
+public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+
+        double salarioMinimo = 2500.33;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        // short numeroCurto2 = numeroNormal; -> Não iria funcionar, pois o número normal é um int e o número curto é um short, e o Java não tem como adivinhar se o número normal vai caber dentro do número curto. Para resolver isso, precisamos fazer um cast, ou seja, dizer para o Java que temos a certeza que o número normal vai caber dentro do número curto. 
+        short numeroCurto2 = (short) numeroNormal;
     }
 }
